fa7c6953c93a24ad0faa369d3923e927710da1f4
Charlie <charlievo22@gmail.com>
2022-02-06 16:59:16 +0100

    Created Controller and API endpoints for getAll and getId

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+            
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = context.Products.ToList();
+            return Ok(products);
+        }
+        
+        [HttpGet("{id}")] // access example: api/products/3
+        public ActionResult<Product> GetProducts(int id)
+        {
+            return context.Products.Find(id);
+        }
+
+    }
+}
