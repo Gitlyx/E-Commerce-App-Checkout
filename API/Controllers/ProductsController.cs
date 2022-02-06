7b81426237dfa590bdb9df381c040723f28d3801
Charlie <charlievo22@gmail.com>
2022-02-06 20:24:15 +0100

    Initialized React Typescript

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,7 +8,7 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
-{
+{ 
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
