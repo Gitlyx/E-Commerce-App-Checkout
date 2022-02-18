bd43edeb59ec55299518088699efef9c5c439b2b
Charlie Vo <charlievo22@gmail.com>
2022-02-18 17:04:25 +0100

    Added requests

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -3,3 +3,21 @@ import axios, { AxiosResponse } from "axios";
 axios.defaults.baseURL = "http://localhost:5000/api/";
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
+};
+
+const Catalog = {
+  list: () => requests.get("products"),
+  details: (id: number) => requests.get(`products/${id}`),
+};
+
+const agent = {
+  Catalog,
+};
+
+export default agent;
